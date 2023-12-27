44d6e25aec7cbfcf2fce9529d7e5ff20e4cc17ab
slee <sanlee8495@gmail.com>
2023-12-27 12:12:50 -0800

    12272023

--- FEFO_data.R
+++ FEFO_data.R
@@ -9,7 +9,7 @@ library(stringr)
 # Main Data (raw)
 
 ######################## make sure to text this in & out back and fourth #################################
-# fefo_df_xlsx <- readxl::read_excel("C:/Users/slee/OneDrive - Ventura Foods/Ventura Work/SCE/Project/FY 24/FEFO Non Compliance Reporting/Weekly Report/2023/12.18.2023/fefo_report.xlsx")
+# fefo_df_xlsx <- readxl::read_excel("C:/Users/slee/OneDrive - Ventura Foods/Ventura Work/SCE/Project/FY 24/FEFO Non Compliance Reporting/Weekly Report/2023/12.27.2023/fefo_report.xlsx")
 # saveRDS(fefo_df_xlsx, "fefo_df.rds")
 
 
